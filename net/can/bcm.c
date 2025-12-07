8f7fbb8f6ed5edc1408dadd12f77f5f8662bffd4
manh-td <dmtran120901@gmail.com>
2025-12-07 17:43:52 +0800

    Fix delay release of struct bcm_op after synchronize_rcu()

--- net/can/bcm.c
+++ net/can/bcm.c
@@ -774,6 +774,7 @@ static int bcm_delete_rx_op(struct list_head *ops, canid_t can_id, int ifindex)
 						  bcm_rx_handler, op);
 
 			list_del(&op->list);
+			synchronize_rcu();
 			bcm_remove_op(op);
 			return 1; /* done */
 		}
@@ -1470,9 +1471,12 @@ static int bcm_release(struct socket *sock)
 			can_rx_unregister(NULL, op->can_id,
 					  REGMASK(op->can_id),
 					  bcm_rx_handler, op);
-
-		bcm_remove_op(op);
 	}
+
+	synchronize_rcu();
+
+	list_for_each_entry_safe(op, next, &bo->rx_ops, list)
+		bcm_remove_op(op);
 
 	/* remove procfs entry */
 	if (proc_dir && bo->bcm_proc_read)
