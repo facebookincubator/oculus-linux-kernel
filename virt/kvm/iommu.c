3f90ccb83d91b671a7380bedd55aeca978d40dfc
pr-hung <bkedx0000@gmail.com>
2025-12-25 03:33:41 +0800

    Fix potential vulnerability in cloned code (virt/kvm/iommu.c)

--- virt/kvm/iommu.c
+++ virt/kvm/iommu.c
@@ -131,6 +131,7 @@ int kvm_iommu_map_pages(struct kvm *kvm, struct kvm_memory_slot *slot)
 		if (r) {
 			printk(KERN_ERR "kvm_iommu_map_address:"
 			       "iommu failed to map pfn=%llx\n", pfn);
+			kvm_unpin_pages(kvm, pfn, page_size);
 			kvm_unpin_pages(kvm, pfn, page_size >> PAGE_SHIFT);
 			goto unmap_pages;
 		}
