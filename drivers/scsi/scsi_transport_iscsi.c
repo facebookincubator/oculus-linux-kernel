e769126ce81d9aefcfcfa0755c96334cea185096
gogogobot <mr.copy.tuo@gmail.com>
2026-02-28 21:23:34 +0800

    scsi: iscsi: Verify lengths on passthrough PDUs

--- drivers/scsi/scsi_transport_iscsi.c
+++ drivers/scsi/scsi_transport_iscsi.c
@@ -2569,6 +2569,7 @@ iscsi_if_send_reply(uint32_t group, int seq, int type, int done, int multi,
 static int
 iscsi_if_get_stats(struct iscsi_transport *transport, struct nlmsghdr *nlh)
 {
+	u32 pdu_len;
 	struct iscsi_uevent *ev = nlmsg_data(nlh);
 	struct iscsi_stats *stats;
 	struct sk_buff *skbstat;
@@ -3600,6 +3601,13 @@ iscsi_if_recv_msg(struct sk_buff *skb, struct nlmsghdr *nlh, uint32_t *group)
 			err = -EINVAL;
 		break;
 	case ISCSI_UEVENT_SEND_PDU:
+		pdu_len = nlh->nlmsg_len - sizeof(*nlh) - sizeof(*ev);
+
+		if ((ev->u.send_pdu.hdr_size > pdu_len) ||
+		    (ev->u.send_pdu.data_size > (pdu_len - ev->u.send_pdu.hdr_size))) {
+			err = -EINVAL;
+			break;
+		}
 		conn = iscsi_conn_lookup(ev->u.send_pdu.sid, ev->u.send_pdu.cid);
 		if (conn)
 			ev->r.retcode =	transport->send_pdu(conn,
