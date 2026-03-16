eb22ee9c7d102c810c904ff3d744464c176e04c7
npt-1707 <npthanh132@gmail.com>
2026-03-17 04:07:12 +0800

    random32: update the net random state on interrupt and activity

--- include/linux/random.h
+++ include/linux/random.h
@@ -7,6 +7,7 @@
 #define _LINUX_RANDOM_H
 
 #include <uapi/linux/random.h>
+#include <linux/percpu.h>
 
 extern void add_device_randomness(const void *, unsigned int);
 extern void add_input_randomness(unsigned int type, unsigned int code,
@@ -34,6 +35,8 @@ void prandom_reseed_late(void);
 struct rnd_state {
 	__u32 s1, s2, s3, s4;
 };
+
+DECLARE_PER_CPU(struct rnd_state, net_rand_state) __latent_entropy;
 
 u32 prandom_u32_state(struct rnd_state *state);
 void prandom_bytes_state(struct rnd_state *state, void *buf, size_t nbytes);
